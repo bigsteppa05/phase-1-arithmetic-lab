3443f54afa570049cff267109a5df361c1690dcb
bigsteppa05 <habibforderiv05@gmail.com>
2024-03-19 10:23:39 +0300

    done

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
-
-// Write your code here
+let num1 = 31;
+let num2 = 2;
+let multiply = num1 * num2;
+let random = Math.floor(Math.random() * 100) + 1;
+let num3 = 64;
+let num4 = 16;
+let mod = num3 % num4;
+let max = Math.max(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20);
+console.log("multiply: " , multiply);
+console.log("random: " , random);
+console.log("mod: " , mod);
+console.log("max: " , max);
